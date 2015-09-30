e3f4a913e02c3f7cf28f68bd1ddbea137959f227
Mike <mikekh@uw.edu>
2015-09-30 14:04:38 -0700

    This intial files

---
+++ game.js
@@ -0,0 +1,13 @@
+var question1 = prompt('First Question. Have I lived outside the USA?');
+var question2 = prompt('Next question. Do you like spam?');
+var question3 = prompt('Final question. Do I have any siblings?');
+
+console.log('Question 1 response: ' + question1);
+console.log('Question 2 response: ' + question2);
+console.log('Question 3 response: ' + question3);
+
+alert('Your Answer: ' + question1 + ' \nThe correct answer is: Yes. \n\nI lived in South Korea.')
+alert('Your Answer: ' + question2 + ' \nThere is no correct answer.\n\nSpam is a very popular item in Hawaii and South Korea.')
+alert('Your Answer: ' + question3 + ' \nThe correct answer is: Yes. \n\nI have a sister and 2 brothers.')
+
+
