af6daa9f7c5af94e962d6e74c6b7fc2be1e71a4b
Mike <mikekh@uw.edu>
2015-09-30 16:24:33 -0700

    Add tally variable and greeting prompt

--- game.js
+++ game.js
@@ -1,34 +1,41 @@
+var tally = 0; //This is the number they would have gotten correct
+
+var userName = prompt('Please enter your name')
+alert('Hi ' + userName +"!" + ' Welcome to my guessing game.')
+
 var question1 = prompt('First Question. Have I lived outside the USA? Yes/No');
 if (question1.toUpperCase() === "YES" ||
     question1.toUpperCase() ==="Y" ||
     question1.toUpperCase() === "YEAH"){
   alert("Good Job! You got it right!");
+  tally++;
   } else {
-    alert("You guessed wrong.")
+    alert("You guessed wrong.");
   }
 
-
-
-
-var question2 = prompt('Next question. Do you like spam?');
+var question2 = prompt('Next question. Do you like spam? Yes/No');
 if (question2.toUpperCase() === "YES" ||
     question2.toUpperCase() ==="Y" ||
     question2.toUpperCase() === "YEAH"){
   alert("Awesome! Spam is great!");
+  tally++;
   } else {
-    alert("Wrong answer. Spam is great.")
+    alert("Wrong answer. Spam is great.");
   }
 
-var question3 = prompt('Final question. Do I have any siblings?');
+var question3 = prompt('Final question. Do I have any siblings? Yes/No');
 if (question3.toUpperCase() === "YES" ||
     question3.toUpperCase() === "Y" ||
     question3.toUpperCase() === "YEAH") {
-  alert("Correct! I have a sister and 2 brothers.")
+  alert("Correct! I have a sister and 2 brothers.");
+  tally++;
 } else {
-  alert("Wong. I have a sister and 2 brothers")
+  alert("Wrong. I have a sister and 2 brothers.")
 }
+
 
 console.log('Question 1 response: ' + question1);
 console.log('Question 2 response: ' + question2);
 console.log('Question 3 response: ' + question3);
+console.log('Total correct:' + tally);
 
